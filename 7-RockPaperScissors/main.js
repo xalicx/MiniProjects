ca1948c06d65270e5fb22303387b0a8f31eb6a3e
alapio <alicjapiotrowicz123@gmail.com>
2024-11-15 11:57:03 +0100

    reset player choice and pushing score

--- 7-RockPaperScissors/main.js
+++ 7-RockPaperScissors/main.js
@@ -39,6 +39,36 @@ function computerChoice() {
   return aiHand;
 }
 
+function publishResult(player, ai, result) {
+  document.querySelector('[data-summary="your-choice"]').textContent = player;
+  document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+
+  document.querySelector(".numbers span").textContent = ++gameSummary.numbers;
+
+  if (result === "win") {
+    document.querySelector(".wins span").textContent = ++gameSummary.wins;
+    document.querySelector('[data-summary="who-win"]').textContent =
+      "you won!:)";
+    document.querySelector('[data-summary="who-win"]').style.color = "green";
+  } else if (result === "loss") {
+    document.querySelector(".losses span").textContent = ++gameSummary.losses;
+    document.querySelector('[data-summary="who-win"]').textContent =
+      "AI won :/";
+    document.querySelector('[data-summary="who-win"]').style.color = "red";
+  } else {
+    document.querySelector(".draws span").textContent = ++gameSummary.draws;
+    document.querySelector('[data-summary="who-win"]').textContent =
+      "it's a draw :<";
+    document.querySelector('[data-summary="who-win"]').style.color = "orange";
+  }
+}
+
+function endGame() {
+  document.querySelector(`[data-option = ${game.playerHand}]`).style.boxShadow =
+    "";
+  game.playerHand = "";
+}
+
 function startGame() {
   if (!game.playerHand) {
     alert("Choose hand");
@@ -47,6 +77,8 @@ function startGame() {
   game.aiHand = computerChoice();
   const gameResult = checkResult(game.playerHand, game.aiHand);
   console.log(gameResult);
+  publishResult(game.playerHand, game.aiHand, gameResult);
+  endGame();
 }
 
 hands.forEach((hand) => hand.addEventListener("click", handSelection));
